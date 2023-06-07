fe7f8160cf3dcc003c67d610e8fc1e8f72f249a8
ryannicholas <ryannicholas.vieira@gmail.com>
2023-06-07 20:23:45 -0300

    feat: :sparkles: add trending desktop layout

--- web/src/components/Trending.tsx
+++ web/src/components/Trending.tsx
@@ -3,6 +3,7 @@
 import styled from "styled-components"
 import React, { useRef, useState, useEffect } from "react";
 import { api } from '@/lib/api';
+import { Play } from 'lucide-react';
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -43,6 +44,28 @@ export default function Trending() {
                 <div className="numeration">
                   {String(index + 1).padStart(2, '0')}
                 </div>
+                <div className="identification">
+                  <h1>{slide.title.romaji}</h1>
+                  <h2>{String(index + 1).padStart(2, '0')}</h2>
+                </div>
+                <Info className="hide">
+                  <h1>{slide.title.romaji}</h1>
+                  <h2 className="description">{slide.description}</h2>
+                  <h2>Japanese: <span>Watashi</span></h2>
+                  <h2>Synonyms: <span>Demon Slayer, Kimetsu no Yaiba</span></h2>
+                  <h2>Aired: <span>Apr 9, 2023</span></h2>
+                  <h2>Status: <span>Currently Airing</span></h2>
+                  <h2 className="genres">Genres: <span> Action, Adventure, Demons, Historical, Shounen, Super Power</span></h2>
+                  <div className="buttons">
+                    <button className="watch">
+                      <Play />
+                      Watch Now
+                    </button>
+                    <button className="add">
+                      +
+                    </button>
+                  </div>
+                </Info>
               </Slide>
             </SwiperSlide>
           ))
@@ -53,6 +76,16 @@ export default function Trending() {
 }
 
 const Container = styled.div`
+  cursor: grab;
+  cursor: -moz-grab;
+  cursor: -webkit-grab;
+
+  :active{
+    cursor: grabbing;
+    cursor: -moz-grabbing;
+    cursor: -webkit-grabbing;
+  }
+
   .swiper-slide {
     display: flex;
     align-items: center;
@@ -61,11 +94,19 @@ const Container = styled.div`
     height: 133px;
     flex-shrink: 0;
   }
+
+  @media (min-width: 776px) {
+    margin-left: 20px;
+      .swiper-slide{
+        width: 18% !important;
+        height: 36vh;
+      }
+    }
 `
 
 const Slide = styled.div<Slide>`
-  width: 89px;
-  height: 133px;
+  width: 100%;
+  height: 100%;
   background-image: url(${(props) => props.backgroundImage});
   background-repeat: no-repeat;
   background-size: cover;
@@ -89,5 +130,132 @@ const Slide = styled.div<Slide>`
 
     font-size: 18px;
     font-weight: bolder;
+  }
+
+  .identification, .hide {
+    display: none;
+  }
+
+  @media (min-width: 776px) {
+    margin-right: 70px;
+    cursor: pointer;
+    .numeration {
+      display: none;
+    }
+
+    .identification {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: end;
+      width: 40px;
+      height: 100%;
+      background-color: #4a4b51;
+      background: linear-gradient(180deg,#4a4b51 0,rgba(74,75,81,0) 99%);
+
+      position: absolute;
+      left: -40px;
+
+      h1 {
+        writing-mode: vertical-rl;
+        transform: rotate(180deg);
+        color: #fff;
+        margin-bottom: 12px;
+        white-space: nowrap;
+        height: 30vh;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+
+      h2 {
+        color: crimson;
+        margin-bottom: 6px;
+        font-size: 18px;
+        font-weight: bolder;
+      }
+    }
+
+    &:hover .hide {
+      display: flex;
+    }
+  }
+`
+
+const Info = styled.div`
+  flex-direction: column;
+  width: 360px;
+  height: 100%;
+
+  padding: 12px;
+
+  background-color: #343a40;
+  border-radius: 12px;
+
+  position: absolute;
+  left: 50%;
+ 
+  z-index: 1;
+
+  h1 {
+    color: #fff;
+    word-wrap: break-word;
+    overflow: hidden;
+    height: 34px;
+    margin-bottom: 12px;
+  }
+
+  .description {
+    height: 72px;
+    margin-bottom: 12px;
+  }
+
+  .genres {
+    height: 32px;
+  }
+
+  h2 {
+    display: flex;
+    color: #ccc;
+    word-wrap: break-word;
+    overflow: hidden;
+    font-size: 12px;
+    margin-bottom: 6px;
+    line-height:15px;
+  }
+
+  span {
+    color: #fff;
+    height: 14px;
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    height: 40px;
+    margin-top: 20px;
+
+    .watch, .add {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      font-weight: bolder;
+      border: none;
+    }
+
+    .watch {
+      width: 70%;
+
+      background-color: crimson;
+      border-radius: 25px;
+      color: #fff;
+    }
+
+    .add {
+      width: 40px;
+      background-color: #fff;
+      border-radius: 50%;
+    }
   }
 `
